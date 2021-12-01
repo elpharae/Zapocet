866b25f0a9d40f430cabd87fe472b5fda76ddd24
elpharaexd <elpharaexd@outlook.com>
2021-12-01 12:30:29 +0100

    Done

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -1,11 +1,22 @@
 package pkg;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static final Scanner SC = new Scanner(System.in);
+
     public static void main(String[] args) {
-	    Matice matice = new Matice(2, 10);
+        int rozmerMatice = 0;
+        while (!(rozmerMatice > 0 && rozmerMatice <= 10)) {
+            System.out.print("Zadej rozměr matice: ");
+            rozmerMatice = Main.SC.nextInt();
+        }
+        System.out.print("Zadej hranici intervalu: ");
+        int hraniceIntervalu = Math.abs(Main.SC.nextInt());
 
         try {
+            Matice matice = new Matice(rozmerMatice, hraniceIntervalu);
             matice.vypis();
             System.out.println(matice.maximumNadHD());
         } catch (NelzeUrcitException e) {
